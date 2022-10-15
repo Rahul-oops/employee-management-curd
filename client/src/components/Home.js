aa931d2ba44bc1bf451b5c77a4bbf55e96d9b125
Rahul_oops <rahuldas20042000@gmail.com>
2022-10-15 11:13:50 +0530

    minor fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 import { adddata, deldata } from "./context/ContextProvider";
 import { updatedata } from "./context/ContextProvider";
 
-const API_ENDPOINT = window.href;
+const API_ENDPOINT = "";
 
 const Home = () => {
   const [getuserdata, setUserdata] = useState([]);
